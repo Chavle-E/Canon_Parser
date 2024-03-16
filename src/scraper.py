4ce79ed7011c02ce27d886722fcc3ceb2432ac37
Chavle-E <chavleshviliguka@gmail.com>
2024-03-16 05:09:52 +0400

    Fixed Logic for pagination

--- src/scraper.py
+++ src/scraper.py
@@ -12,15 +12,17 @@ def scrape_canon_preview(category, driver):
     scroll_to_load_more(driver, "//button[@class='primary amscroll-load-button' and @amscroll_type='after']")
     page_source = driver.page_source
     soup = BeautifulSoup(page_source, 'html.parser')
-    product_divs = soup.find_all('div', {'id': 'amasty-shopby-product-list'})
-    for product_div in product_divs:
-        ol = product_div.find('ol', {'id': 'product-items'})
-        cameras = ol.find_all('li')
-        for camera in cameras:
-            name_h2 = camera.find('h2', class_='product name product-item-name')
-            model = name_h2.find('a', class_='product-item-link').text.strip()
-            print(model)
+    amscroll_divs = {}
+    for i in range(1, 4):
+        amscroll_divs[f'page_{i}'] = soup.find_all('div', attrs={'amscroll-page': str(i)})
+
+    print(amscroll_divs)
 
 
-
-
+    # for product_div in product_divs:
+    #     ol = product_div.find('ol', {'id': 'product-items'})
+    #     cameras = ol.find_all('li')
+    #     for camera in cameras:
+    #         name_h2 = camera.find('h2', class_='product name product-item-name')
+    #         model = name_h2.find('a', class_='product-item-link').text.strip()
+    #         print(model)
--- src/selenium_utils.py
+++ src/selenium_utils.py
@@ -2,7 +2,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver import ActionChains
 import time
 
 
@@ -13,20 +12,22 @@ def wait_for_page_load(driver):
 
 
 def find_load_more(driver, xpath):
-    return driver.find_element(By.XPATH, xpath)
+    return WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, xpath))
+            )
 
 
 def scroll_to_load_more(driver, xpath):
     while True:
         try:
             load_more_button = find_load_more(driver, xpath)
-            action = ActionChains(driver)
             if load_more_button:
-                print("Attempting to scroll to the 'Load More' button...")
-                driver.execute_script("arguments[0].scrollIntoView({block: 'center', inline: 'center'});", load_more_button)
-                action.move_to_element(load_more_button).click()
+                driver.execute_script(
+                    "arguments[0].scrollIntoView({behavior: 'smooth', block: 'center', inline: 'center'});",
+                    load_more_button)
                 time.sleep(3)
-                action.perform()
+                driver.execute_script("arguments[0].click();", load_more_button)
+                time.sleep(3)
             else:
                 print("No more 'Load More' buttons to click.")
                 break
